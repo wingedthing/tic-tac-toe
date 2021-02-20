a63d9985008c6f4c39e311e46a99054257ded8ee
John Sfara <jcarlo@gmail.com>
2021-02-20 10:15:51 -0500

    Add clearBoard function to gameBoard module. Refactor DOM access to one instance where all td elemenst are selected only once.

--- app.js
+++ app.js
@@ -8,25 +8,49 @@
   const gameBoard = (() => {
     let _board = new Array(9);
     const getSquare = (indexNum) => _board[indexNum];
+    const _htmlSquares = document.querySelectorAll('td');
 
     /**
      * Sets the textContent of a square to the value of the player's symbol (X or O)
-     * @param {*} indexNum data index number of the square in the array from top left to bottom right 0-8.
+     * @param {*} indexNum id number of the square in the array from top left to bottom right 0-8.
      * @param {*} player the player who changes the textContent. 
      */
     const setSquare = (indexNum, player) => {
-      const htmlSquare = document.getElementById(`${indexNum}`);
-      
-      
+      const htmlSquare = _htmlSquares[indexNum];
+      htmlSquare.textContent = player.getSymbol();
+      _board[indexNum] = player.getSymbol();
+
+    }
+
+    const clearBoard = () => {
+      _board.forEach((element, index) =>{ 
+        _board[index] = undefined
+        _htmlSquares[index].textContent = '';
+      });
+    
     }
 
     return {
       getSquare,
-      setSquare
+      setSquare,
+      clearBoard
     }
-
-
   })();
+
+  /**
+   * 
+   * @param {*} symbol the player's symbol (X or O);
+   */
+  const playerFactory = (symbol) => {
+    let _symbol = symbol;
+    const getSymbol = () => _symbol;
+
+
+    return {
+      getSymbol,
+
+    }  
+  }
 
 
 })();
